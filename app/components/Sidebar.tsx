98d4aa6af24d0cecb297f7e779b2932ae45c4fad
Priyanka Mehta <primehta17@gmail.com>
2026-05-12 21:55:30 +0530

    first commit

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+      <h1>logo</h1>
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
