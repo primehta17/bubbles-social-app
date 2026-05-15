01426b7fb5e243df37ac27cb364a1d21524f0db7
Priyanka Mehta <primehta17@gmail.com>
2026-05-15 23:53:58 +0530

    footer login signup modal redux materialui

--- app/components/modals/SignUpModal.tsx
+++ app/components/modals/SignUpModal.tsx
@@ -1,6 +1,9 @@
 'use client'
 import React, { useState } from 'react';
 import { Modal } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState,AppDispatch } from '@/app/redux/store'
+import { closeSignUpModal, openSignUpModal } from '@/app/redux/slices/modalSlice';
 
 function SignUpModal() {
   // const [isOpen, setIsOpen] = useState(true);
@@ -10,12 +13,18 @@ function SignUpModal() {
   // const handleOpen=()=>{
   //   setIsOpen(true)
   // }
+
+  const isOpen = useSelector((state:RootState)=>
+    state.modals.signUpModalOpen
+  );
+
+  const dispatch:AppDispatch = useDispatch()
   return (
     <div>
-      {/* <button className='w-full h-[47px] md:w-[90px] md:h-[40px] md:text-sm text-lg font-bold bg-white rounded-full' onClick={handleOpen}> Sign Up</button>
-      <Modal open={isOpen} onClose={handleClose} className='flex justify-center items-center'>
-        <div className="w-[200px] h-[400px] bg-white">hhhj</div>
-      </Modal> */}
+      <button className='w-full h-[47px] md:w-[90px] md:h-[40px] md:text-sm text-lg font-bold bg-white rounded-full' onClick={()=>{dispatch(openSignUpModal())}}> Sign Up</button>
+      <Modal open={isOpen} onClose={()=>dispatch(closeSignUpModal())} className='flex justify-center items-center'>
+        <div className="w-[200px] h-[400px] bg-white"></div>
+      </Modal>
     </div>
    
   )
